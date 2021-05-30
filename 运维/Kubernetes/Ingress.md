ff168adb6f1afd26a471090c0379c7343f7f1665
zanpocc <zanpocc@gmail.com>
2021-05-31 01:03:10 +0800

    update files

---
+++ 运维/Kubernetes/Ingress.md
@@ -0,0 +1,14 @@
+# Ingress
+
+## 1、安装启动Ingress
+
+
+
+
+
+## 2、创建Ingress
+
+
+
+
+
---
+++ 运维/Linux/查看系统版本.sh
@@ -0,0 +1,14 @@
+# 1、Linux内核版本
+cat /proc/version
+
+# 2、Linux内核版本
+uname -a
+
+# 3、系统版本
+lsb_release -a
+
+# 4 只适合Redhat系的Linux
+cat /etc/redhat-release
+
+# 5、系统版本
+cat /etc/issue
